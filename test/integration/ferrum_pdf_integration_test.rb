030580091cdaa1500e5ef518c3e45e66b6e8d52a
Zakaria Fatahi <zakaria@buzzjective.com>
2024-10-09 18:09:09 +0330

    Add integration tests for PDF and PNG generation
    - Create ferrum_pdf_integration_test.rb
    - Test PDF generation from view template and URL
    - Test PNG screenshot generation from view template and URL

---
+++ test/integration/ferrum_pdf_integration_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class FerrumPdfIntegrationTest < ActionDispatch::IntegrationTest
+  test "can generate a PDF" do
+    get "/pdfs/show.pdf"
+    assert_response :success
+    assert_equal "application/pdf", response.content_type
+    assert_includes response.headers["Content-Disposition"], "inline"
+    assert_includes response.headers["Content-Disposition"], 'filename="example.pdf"'
+  end
+
+  test "can generate a PNG screenshot" do
+    get "/pdfs/show.png"
+    assert_response :success
+    assert_equal "image/png", response.content_type
+    assert_includes response.headers["Content-Disposition"], "inline"
+    assert_includes response.headers["Content-Disposition"], 'filename="example.png"'
+  end
+
+  test "can generate a PDF from URL" do
+    get "/pdfs/url.pdf", params: { url: "http://example.com" }
+    assert_response :success
+    assert_equal "application/pdf", response.content_type
+    assert_includes response.headers["Content-Disposition"], "inline"
+    assert_includes response.headers["Content-Disposition"], 'filename="example.pdf"'
+  end
+
+  test "can generate a PNG screenshot from URL" do
+    get "/pdfs/url.png", params: { url: "http://example.com" }
+    assert_response :success
+    assert_equal "image/png", response.content_type
+    assert_includes response.headers["Content-Disposition"], "inline"
+    assert_includes response.headers["Content-Disposition"], 'filename="example.png"'
+  end
+end
