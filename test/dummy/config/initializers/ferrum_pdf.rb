c25e3d66788971d7fc49951effe80255a6204db3
Zakaria Fatahi <zakaria@buzzjective.com>
2024-10-15 19:01:40 +0330

    Updated the dummy application's initializer
    - To  use the new Rails-like configuration syntax.
    - Update its tests too

--- test/dummy/config/initializers/ferrum_pdf.rb
+++ test/dummy/config/initializers/ferrum_pdf.rb
@@ -1,5 +1,5 @@
 FerrumPdf.configure do |config|
-  config[:headless] = true
-  config[:timeout] = 20
-  config[:window_size] = [ 1280, 800 ]
+  config.headless = true
+  config.timeout = 20
+  config.window_size = [ 1280, 800 ]
 end
--- test/ferrum_pdf_initializer_test.rb
+++ test/ferrum_pdf_initializer_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class FerrumPdfInitializerTest < ActiveSupport::TestCase
   test "initializer sets configuration" do
     # This test checks the configuration set by the initializer in the dummy app
-    assert_equal true, FerrumPdf.configuration[:headless]
-    assert_equal 20, FerrumPdf.configuration[:timeout]
-    assert_equal [ 1280, 800 ], FerrumPdf.configuration[:window_size]
+    assert_equal true, FerrumPdf.configuration.headless
+    assert_equal 20, FerrumPdf.configuration.timeout
+    assert_equal [ 1280, 800 ], FerrumPdf.configuration.window_size
   end
 end
