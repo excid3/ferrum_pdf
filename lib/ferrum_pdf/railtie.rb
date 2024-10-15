c800e1c1077e1f13e9b67e96ea9ea5b36e4fac3b
Zakaria Fatahi <zakaria@buzzjective.com>
2024-10-15 18:55:28 +0330

    Removed an unnecessary initializer from the Railtie.

--- lib/ferrum_pdf/railtie.rb
+++ lib/ferrum_pdf/railtie.rb
@@ -5,13 +5,5 @@ module FerrumPdf
         include FerrumPdf::Controller if FerrumPdf.include_controller_module
       end
     end
-
-    initializer "ferrum_pdf.load_configuration" do
-      config.after_initialize do
-        FerrumPdf.configure do |config|
-          # Load default configuration here if needed
-        end
-      end
-    end
   end
 end
