f0acf32d3fc643028adb1d66eb7f7b17b8354b1c
Zakaria Fatahi <zakaria@buzzjective.com>
2024-10-15 19:00:19 +0330

    Updated the test suite
    - to always use headless mode, as non-headless mode is only needed for debugging.
    - Use the Rails-like configuration syntax

--- test/ferrum_pdf_configuration_test.rb
+++ test/ferrum_pdf_configuration_test.rb
@@ -7,45 +7,37 @@ class FerrumPdf::ConfigurationTest < ActiveSupport::TestCase
 
   test "configuration can be set through configure method" do
     FerrumPdf.configure do |config|
-      config[:headless] = true
-      config[:window_size] = [ 1024, 768 ]
-      config[:timeout] = 30
+      config.timeout = 30
+      config.window_size = [ 1024, 768 ]
     end
 
-    assert_equal true, FerrumPdf.configuration[:headless]
-    assert_equal 30, FerrumPdf.configuration[:timeout]
-    assert_equal [ 1024, 768 ], FerrumPdf.configuration[:window_size]
+    assert_equal 30, FerrumPdf.configuration.timeout
+    assert_equal [ 1024, 768 ], FerrumPdf.configuration.window_size
   end
 
   test "configuration is used when creating browser" do
     FerrumPdf.configure do |config|
-      config[:headless] = true
-      config[:timeout] = 45
+      config.timeout = 45
     end
 
     browser = FerrumPdf.browser
     assert_instance_of Ferrum::Browser, browser
-    assert_equal true, browser.instance_variable_get(:@options).headless
     assert_equal 45, browser.instance_variable_get(:@options).timeout
   end
 
   test "changing configuration creates new browser instance" do
     FerrumPdf.configure do |config|
-      config[:headless] = true
-      config[:timeout] = 30
+      config.timeout = 20
     end
     first_browser = FerrumPdf.browser
 
     FerrumPdf.configure do |config|
-      config[:headless] = true
-      config[:timeout] = 60
+      config.timeout = 30
     end
     second_browser = FerrumPdf.browser
 
     assert_not_equal first_browser.object_id, second_browser.object_id
-    assert_equal true, first_browser.instance_variable_get(:@options).headless
-    assert_equal true, second_browser.instance_variable_get(:@options).headless
-    assert_equal 30, first_browser.instance_variable_get(:@options).timeout
-    assert_equal 60, second_browser.instance_variable_get(:@options).timeout
+    assert_equal 20, first_browser.instance_variable_get(:@options).timeout
+    assert_equal 30, second_browser.instance_variable_get(:@options).timeout
   end
 end
