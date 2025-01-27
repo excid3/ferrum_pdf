610c557224609fa097b68cc5ab09787c3c5ec167
Javier Aranda <javier@aranda.dev>
2025-01-27 09:46:05 +0100

    Add assets helpers to rails views

--- lib/ferrum_pdf/railtie.rb
+++ lib/ferrum_pdf/railtie.rb
@@ -1,5 +1,11 @@
 module FerrumPdf
   class Railtie < ::Rails::Railtie
+    initializer "ferrum_pdf.assets_helper" do
+      ActiveSupport.on_load(:action_view) do
+        include FerrumPdf::AssetsHelper if FerrumPdf.include_assets_helper_module
+      end
+    end
+
     initializer "ferrum_pdf.controller" do
       ActiveSupport.on_load(:action_controller) do
         include FerrumPdf::Controller if FerrumPdf.include_controller_module
