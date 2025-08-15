6e1c277bdc695bc60288b77d60629bb6f7102255
Viktor Ivliiev <vivliiev@mobilecoach.com>
2025-08-15 22:53:18 +0300

    Removing duplicates, adding tests for assets, general self/private style

--- lib/ferrum_pdf/assets_helper.rb
+++ lib/ferrum_pdf/assets_helper.rb
@@ -27,39 +27,45 @@ module FerrumPdf
 
   class AssetFinder
     class << self
-      def find(path)
-        if Rails.application.assets.respond_to?(:load_path)
-          propshaft_asset(path)
-        elsif Rails.application.assets.respond_to?(:find_asset)
-          sprockets_asset(path)
-        else
-          nil
+      def find(path, assets: Rails.application.assets)
+        if assets.respond_to?(:load_path)
+          propshaft_asset(assets.load_path.find(path))
+        elsif assets.respond_to?(:find_asset)
+          sprockets_asset(assets.find_asset(path))
         end
       end
 
-      def propshaft_asset(path)
-        (asset = Rails.application.assets.load_path.find(path)) ? PropshaftAsset.new(asset) : nil
+      private
+
+      def propshaft_asset(asset)
+        asset && PropshaftAsset.new(asset)
       end
 
-      def sprockets_asset(path)
-        (asset = Rails.application.assets.find_asset(path)) ? SprocketsAsset.new(asset) : nil
+      def sprockets_asset(asset)
+        asset && SprocketsAsset.new(asset)
       end
     end
   end
 
   module AssetsHelper
     def ferrum_pdf_inline_stylesheet(path)
-      (asset = AssetFinder.find(path)) ? "<style>#{asset.content}</style>".html_safe : nil
+      inline_ferrum_pdf_tag path, "style"
     end
 
     def ferrum_pdf_inline_javascript(path)
-      (asset = AssetFinder.find(path)) ? "<script>#{asset.content}</script>".html_safe : nil
+      inline_ferrum_pdf_tag path, "script"
     end
 
     def ferrum_pdf_base64_asset(path)
       return nil unless (asset = AssetFinder.find(path))
 
       "data:#{asset.content_type};base64,#{Base64.encode64(asset.content).gsub(/\s+/, '')}"
+    end
+
+    private
+
+    def inline_ferrum_pdf_tag(path, tag)
+      (asset = AssetFinder.find(path)) && "<#{tag}>#{asset.content}</#{tag}>".html_safe
     end
   end
 end
