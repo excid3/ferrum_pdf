73484d3ed26b253e3a23c84c6fcffbc6eb2e635f
Brendon Muir <brendon@spike.net.nz>
2025-08-23 21:20:54 +1200

    Add display_url support for HTML rendering
    Chrome behaves much nicer when we fake a real request and inject our html via an intercept.

--- lib/ferrum_pdf/railtie.rb
+++ lib/ferrum_pdf/railtie.rb
@@ -13,7 +13,7 @@ module FerrumPdf
           send_data_options = options.extract!(:disposition, :filename, :status)
           url = pdf_options.delete(:url)
           html = render_to_string(**options.with_defaults(formats: [ :html ])) if url.blank?
-          pdf = FerrumPdf.render_pdf(html: html, base_url: request.base_url, url: url, pdf_options: pdf_options)
+          pdf = FerrumPdf.render_pdf(html: html, display_url: request.original_url, url: url, pdf_options: pdf_options)
           send_data(pdf, **send_data_options.with_defaults(type: :pdf))
         end
 
@@ -22,7 +22,7 @@ module FerrumPdf
           send_data_options = options.extract!(:disposition, :filename, :status)
           url = screenshot_options.delete(:url)
           html = render_to_string(**options.with_defaults(formats: [ :html ])) if url.blank?
-          screenshot = FerrumPdf.render_screenshot(url: url, html: html, base_url: request.base_url, screenshot_options: screenshot_options)
+          screenshot = FerrumPdf.render_screenshot(url: url, html: html, display_url: request.original_url, screenshot_options: screenshot_options)
           send_data(screenshot, **send_data_options.with_defaults(type: screenshot_options.fetch(:format, :png)))
         end
       end
